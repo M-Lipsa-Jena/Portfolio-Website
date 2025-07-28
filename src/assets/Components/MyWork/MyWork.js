d399c4643b7358aa9b799d1fb860c314ad4d6ec1
M-Lipsa-jena <mlipsajena@gmail.com>
2025-07-29 03:46:49 +0530

    Fix Mywork image paths for production

--- src/assets/Components/MyWork/MyWork.js
+++ src/assets/Components/MyWork/MyWork.js
@@ -2,37 +2,38 @@ export const MyWork_data = [
     {
         w_no: 1,
         w_name:"Project 1: Weather Fetching Api",
-        w_img: '/Weather Forecast App.jpg',
+        // w_img: '/Weather Forecast App.jpg',
+        w_img: `${import.meta.env.BASE_URL}Weather Forecast App.jpg`,
         w_live: 'https://venerable-eclair-78e541.netlify.app/',
         w_github: "https://github.com/M-Lipsa-Jena/weather-forecast-app" 
     },
     {
         w_no:2,
         w_name:"Project 2: E-Commerce Website",
-        w_img: '/E-commerce.jpg',
+        w_img: `${import.meta.env.BASE_URL}E-commerce.jpg`,
         w_live: "https://m-lipsa-jena.github.io/furniture-ecommerce/",
         w_github: "https://github.com/M-Lipsa-Jena/furniture-ecommerce"
     },
     {
         w_no:3,
         w_name:"Project 3: Movie Api",
-        w_img: '/movie.jpg'
+        w_img: `${import.meta.env.BASE_URL}movie.jpg`
     },
     
     {
         w_no:4,
         w_name:"Project 4: Design & Validate a Registation From",
-        w_img: '/REgistation.jpg'
+        w_img: `${import.meta.env.BASE_URL}REgistation.jpg`
     },
     {
         w_no:5,
         w_name:"Project 5: Design & Validate a Login From ",
-        w_img: '/Login.jpg'
+        w_img: `${import.meta.env.BASE_URL}Login.jpg`
     },
     {
         w_no:6,
         w_name:"Project 6: Design & Validate a Signup From",
-        w_img: '/Signup.jpg'
+        w_img: `${import.meta.env.BASE_URL}Signup.jpg`
     },
 
 ]
