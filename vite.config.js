dcd1ad74aca9412e301619a82ae94f327384f3da
M-Lipsa-jena <mlipsajena@gmail.com>
2025-07-29 05:41:24 +0530

    update for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio-Website/",
+  base: process.env.NODE_ENV === 'production' && process.env.DEPLOY_TARGET === 'GH_PAGES'
+    ? '/Portfolio-Website/'
+    : '/',
   plugins: [react()],
 })
